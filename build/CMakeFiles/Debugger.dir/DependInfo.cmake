09506d6b45b1e42c7fe572073d4524a6bf766423
raz <you@example.com>
2024-07-19 10:47:12 +0000

    remove compiled binaries

--- build/CMakeFiles/Debugger.dir/DependInfo.cmake
+++ build/CMakeFiles/Debugger.dir/DependInfo.cmake
@@ -8,13 +8,17 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/user/projects/debugging/src/elf_reader.cpp" "CMakeFiles/Debugger.dir/src/elf_reader.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/elf_reader.cpp.o.d"
-  "/home/user/projects/debugging/src/elf_runner.cpp" "CMakeFiles/Debugger.dir/src/elf_runner.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/elf_runner.cpp.o.d"
-  "/home/user/projects/debugging/src/main.cpp" "CMakeFiles/Debugger.dir/src/main.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/main.cpp.o.d"
+  "/home/joe/projects/debugger/src/elf_reader.cpp" "CMakeFiles/Debugger.dir/src/elf_reader.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/elf_reader.cpp.o.d"
+  "/home/joe/projects/debugger/src/elf_runner.cpp" "CMakeFiles/Debugger.dir/src/elf_runner.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/elf_runner.cpp.o.d"
+  "/home/joe/projects/debugger/src/main.cpp" "CMakeFiles/Debugger.dir/src/main.cpp.o" "gcc" "CMakeFiles/Debugger.dir/src/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
