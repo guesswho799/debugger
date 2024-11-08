2f052b2854520fdf3d5d85672877fb4280cce5d5
raz <you@example.com>
2024-11-08 21:02:02 +0000

    add strings tab

--- include/elf_reader.hpp
+++ include/elf_reader.hpp
@@ -16,6 +16,7 @@ public:
     NamedSection get_section(std::string section_name);
     NamedSection get_section(std::size_t section_index);
     std::vector<NamedSymbol> get_symbols();
+    std::vector<std::string> get_strings();
     std::vector<NamedSymbol> get_functions();
     NamedSymbol get_function(std::string name);
     std::vector<Disassebler::Line> get_function_code(NamedSymbol function);
@@ -24,11 +25,14 @@ private:
     ElfHeader header_factory();
     std::vector<NamedSection> sections_factory();
     std::vector<NamedSymbol> static_symbols_factory();
+    std::vector<std::string> strings_factory();
 
 private:
     std::ifstream file;
+
 public:
     ElfHeader header;
     std::vector<NamedSection> sections;
     std::vector<NamedSymbol> _static_symbols;
+    std::vector<std::string> _strings;
 };
