19149244c7f9b7393a041b4b2fe7ddde44489fa5
raz <you@example.com>
2025-01-07 22:48:49 +0000

    add semi working runtime function tracer

--- src/breakpoint_hook.cpp
+++ src/breakpoint_hook.cpp
@@ -9,23 +9,29 @@ BreakpointHook::BreakpointHook(uint64_t address, pid_t child_pid):
         _child_pid(child_pid),
         _original_code(Ptrace::get_code(child_pid, address))
 {
+    hook();
+}
+
+void BreakpointHook::hook()
+{
     const uint64_t new_breakpoint_code = (_original_code & (~(uint64_t)0xFF)) | 0xCC;
-    Ptrace::set_code(child_pid, address, new_breakpoint_code);
+    Ptrace::set_code(_child_pid, _address, new_breakpoint_code);
+}
+
+void BreakpointHook::unhook()
+{
+    Ptrace::set_code(_child_pid, _address, _original_code);
 }
 
 BreakpointHook::~BreakpointHook()
 {
-    BEST_EFFORT(Ptrace::set_code(_child_pid, _address, _original_code));
+    BEST_EFFORT(unhook());
 }
 
 bool BreakpointHook::is_hit(int child_status) const
 {
-    if (WIFSTOPPED(child_status))
-    {
-        struct user_regs_struct regs = Ptrace::get_regs(_child_pid);
-	return regs.rip-1 == _address;
-    }
-    return false;
+    if (!WIFSTOPPED(child_status)) return false;
+    return Ptrace::get_regs(_child_pid).rip == _address + 1;
 }
 
 uint64_t BreakpointHook::get_address() const
--- src/elf_runner.cpp
+++ src/elf_runner.cpp
@@ -56,88 +56,48 @@ pid_t ElfRunner::run(std::string file_name)
         char* args[] = {const_cast<char*>(file_name.c_str()), NULL};
         execv(file_name.c_str(), args);
     }
-    // debugger
-    if (wait(&child_status) == -1)
-    {
-        throw CriticalException(Status::elf_runner__wait_failed);
-    }
-    if (WIFEXITED(child_status))
-    {
-        throw CriticalException(Status::elf_runner__child_died);
-    }
    _is_dead = false;
 
     return pid;
 }
 
-bool ElfRunner::_non_blocking_get_to_breakpoints(const std::vector<NamedSymbol>& functions, int child_status)
+void ElfRunner::run_functions(const std::vector<NamedSymbol>& functions)
 {
-    // overwrite breakpoint code with interrupt
-    if (_breakpoints.empty() and _runtime_arguments.empty())
+    int child_status = 0;
+    int status = waitpid(_child_pid, &child_status, WNOHANG | WUNTRACED);
+    if (status == -1) { throw CriticalException(Status::elf_runner__wait_failed); }
+    if (status != 0) { _update_is_dead(child_status); }
+    if (is_dead() or !WIFSTOPPED(child_status)) return;
+    
+    if (_breakpoints.empty())
     {
+	// must reserve to avoid vector reallocations
+	_breakpoints.reserve(functions.size());
         for (const auto& function: functions)
         {
             _breakpoints.emplace_back(function.value, _child_pid);
         }
 	Ptrace::cont(_child_pid);
     }
-
-    // check if child is interupted
-    const auto is_hit = [&](BreakpointHook breakpoint){ return breakpoint.is_hit(child_status); };
-    const auto iterator = std::find_if(_breakpoints.begin(), _breakpoints.end(), is_hit);
-    if (iterator == _breakpoints.end()) return false;
-    _log_function_arguments(functions, iterator->get_address());
-    _breakpoints.erase(iterator);
-
-    // resume child regular flow
-    struct user_regs_struct regs = Ptrace::get_regs(_child_pid);
-    regs.rip--;
-    Ptrace::set_regs(_child_pid, regs);
-
-    Ptrace::cont(_child_pid);
-
-    return true;
-}
-
-std::optional<ElfRunner::runtime_mapping> ElfRunner::run_function(uint64_t address, uint64_t size, std::vector<uint64_t> calls)
-{
-    int child_status = 0;
-    int status = waitpid(_child_pid, &child_status, WNOHANG | WUNTRACED);
-    if (status == -1) { throw CriticalException(Status::elf_runner__wait_failed); }
-    if (status != 0) { _update_is_dead(child_status); }
-
-  //if (is_dead() or !_non_blocking_get_to_address(address, child_status, calls))
-  //{
-  //    return _runtime_mapping;
-  //}
-
-    struct user_regs_struct regs = Ptrace::get_regs(_child_pid);
-    while (regs.rip >= address and regs.rip <= address + size)
+    else
     {
-	_log_step();
-        regs = Ptrace::get_regs(_child_pid);
+        // check if breakpoint is hit
+        const auto breakpoint = std::find_if(_breakpoints.begin(), _breakpoints.end(),
+            [&](const BreakpointHook& breakpoint){ return breakpoint.is_hit(child_status); });
+        if (breakpoint == _breakpoints.end()) return;
+
+	// log function hit
+        _log_function_arguments(functions, breakpoint->get_address());
+
+        // resume child regular flow
+        struct user_regs_struct regs = Ptrace::get_regs(_child_pid);
+        regs.rip--;
+        Ptrace::set_regs(_child_pid, regs);
+        breakpoint->unhook();
+        _log_step();
+        breakpoint->hook();
+        Ptrace::cont(_child_pid);
     }
-    Ptrace::cont(_child_pid);
-
-    return _runtime_mapping;
-}
-
-std::optional<ElfRunner::runtime_arguments> ElfRunner::run_functions(const std::vector<NamedSymbol>& functions)
-{
-    int child_status = 0;
-    int status = waitpid(_child_pid, &child_status, WNOHANG | WUNTRACED);
-    if (status == -1) { throw CriticalException(Status::elf_runner__wait_failed); }
-    if (status != 0) { _update_is_dead(child_status); }
-
-    if (is_dead() or !_non_blocking_get_to_breakpoints(functions, child_status))
-    {
-	return _runtime_arguments;
-    }
-
-    struct user_regs_struct regs = Ptrace::get_regs(_child_pid);
-    Ptrace::cont(_child_pid);
-
-    return _runtime_arguments;
 }
 
 void ElfRunner::_log_step()
